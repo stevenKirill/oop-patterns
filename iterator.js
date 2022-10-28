f0ccd1124a477c99502dbf77f4b833d1d8afabc4
Kirill Pavlovskii <kirill-pavlovskij@rambler.ru>
2022-10-28 19:53:13 +0300

    added new patterns

---
+++ iterator.js
@@ -0,0 +1,46 @@
+class ArrayIterator {
+    constructor(collection) {
+        this.collection = collection;
+        this.currentIndex = 0;
+    }
+
+    next() {
+        return this.collection[this.currentIndex++];
+    }
+
+    hasNext() {
+        return this.currentIndex < this.collection.length;
+    }
+}
+
+class Objectterator {
+    constructor(collection) {
+        this.collection = collection;
+        this.keys = Object.keys(collection);
+        this.currentIndex = 0;
+    }
+
+    next() {
+        return this.collection[this.keys[this.currentIndex++]];
+    }
+
+    hasNext() {
+        return this.currentIndex < this.keys.length;
+    }
+}
+
+const firstCollection = new ArrayIterator(['1','2','3','4','5']);
+
+while(firstCollection.hasNext()) {
+    console.log(firstCollection.next())
+}
+
+const secondCollection = new Objectterator({
+    name: 'kirill',
+    job: 'frontend',
+    hobby: 'coding',
+});
+
+while(secondCollection.hasNext()) {
+    console.log(secondCollection.next())
+}
